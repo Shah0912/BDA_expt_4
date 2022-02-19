6e5ba78cea90663b277c650353b690e2da74d6bb
Aditya Shah <46823878+Shah0912@users.noreply.github.com>
2022-02-19 20:36:27 +0530

    Displays count of numbers

---
+++ DistinctCount/src/WCReducer.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter; 
+  
+public class WCReducer extends MapReduceBase implements Reducer<IntWritable, 
+                                   IntWritable, IntWritable, IntWritable> { 
+  
+    @Override
+    // Reduce Function 
+    public void reduce(IntWritable key, Iterator<IntWritable> value, 
+     OutputCollector<IntWritable, IntWritable> output, Reporter rep) 
+  
+    throws IOException 
+    { 
+  
+    
+    	int counter=0;
+    	while (value.hasNext())  
+        { 
+            IntWritable i = value.next(); 
+            counter+=i.get();
+//            if(counter==1)
+        } 
+    	
+    	output.collect(key, new IntWritable(counter));
+
+       
+      
+    } 
+} 
